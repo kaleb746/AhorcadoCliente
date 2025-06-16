3d51353284a1b4cd04981ee38a16748f1314ca88
kaleb746 <iankaleb746@gmail.com>
2025-06-15 23:43:04 -0600

    Se agrego la llamada notificar fin partida

--- AhorcadoCliente/Utilidades/Constantes.cs
+++ AhorcadoCliente/Utilidades/Constantes.cs
@@ -8,5 +8,8 @@
 
         public static readonly string PATRON_TEXTO_GENERAL = @"[^A-Za-zÁÉÍÓÚÜÑáéíóúüñ0-9\s\-\.,()/#]+$";
         public static readonly string PATRON_CORREO_GENERAL = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+
+
+
     }
 }
